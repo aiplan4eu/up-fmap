89016e0debf808d30bd339e2a37db4deb2053c8d
alee08 <ale.trapasso8@gmail.com>
2023-11-23 14:41:15 +0100

    Updated README

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ class InstallFMAPdevelop(develop):
 
 setup(
     name="up_fmap",
-    version="0.0.12",
+    version="0.0.13",
     description="up_fmap",
     author="Alejandro Torreño, Eva Onaindia and Oscar Sapena",
     author_email="onaindia@dsic.upv.es",
